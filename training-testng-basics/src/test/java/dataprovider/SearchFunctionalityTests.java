5f16a301f83b7798033fdf89d210bc26a86eac4c
Fabiana Apateanu <fabiana.apateanu@endava.com>
2018-01-10 11:20:41 +0200

    Added suite and group run types and init of dataprovider example.

--- training-testng-basics/src/test/java/dataprovider/SearchFunctionalityTests.java
+++ training-testng-basics/src/test/java/dataprovider/SearchFunctionalityTests.java
@@ -1,6 +1,5 @@
 package dataprovider;
 
-import basic.IgnoreTestsExample;
 import common.CustomDriver;
 import common.CustomLogger;
 import org.apache.log4j.Logger;
@@ -21,18 +20,18 @@ public class SearchFunctionalityTests {
 
     private static Logger LOG;
 
-    @BeforeClass
+    @BeforeClass(groups = {"positive_tests", "negative_tests"})
     public static void runBeforeClassInit() {
-        LOG = CustomLogger.getInstance(IgnoreTestsExample.class).getLogger();
+        LOG = CustomLogger.getInstance(SearchFunctionalityTests.class).getLogger();
         LOG.info("Running setup before class test methods initialization");
     }
 
-    @AfterClass
+    @AfterClass(groups = {"positive_tests", "negative_tests"})
     public static void runAfterClassFinished() {
         LOG.info("Running teardown after class test methods run finished");
     }
 
-    @BeforeMethod
+    @BeforeMethod(groups = {"positive_tests", "negative_tests"})
     public void runBeforeEachTestMethod() {
         LOG.info("Running setup before each test method");
 
@@ -42,13 +41,13 @@ public class SearchFunctionalityTests {
         myDriver = CustomDriver.getInstance();
     }
 
-    @AfterMethod
+    @AfterMethod(groups = {"positive_tests", "negative_tests"})
     public void runAfterEachTestMethod() {
         LOG.info("Running teardown before each test method");
         myDriver.closeDriver();
     }
 
-    @Test
+    @Test(groups = "positive_tests")
     public void loginAndSearchMultipleResults() {
         LoginPage loginPage = new LoginPage(myDriver.getDriver());
         HomePage homePage = loginPage.performLogin(username, password);
@@ -60,8 +59,7 @@ public class SearchFunctionalityTests {
         Assert.assertTrue(homePage.isRepositorySearchResultListDisplayed(), "The search action did not return multiple results");
     }
 
-
-    @Test
+    @Test(groups = "negative_tests")
     public void loginAndSearchNoResults() {
         LoginPage loginPage = new LoginPage(myDriver.getDriver());
         HomePage homePage = loginPage.performLogin(username, password);
