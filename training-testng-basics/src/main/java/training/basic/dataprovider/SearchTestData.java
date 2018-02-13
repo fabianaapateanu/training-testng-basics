790b040fabc0b5cbfed9b1faf244558dbc5f0cb5
Andrada Scrobota <andrada.scrobota@endava.com>
2018-02-13 15:19:13 +0200

    My first commit

---
+++ training-testng-basics/src/main/java/training/basic/dataprovider/SearchTestData.java
@@ -0,0 +1,15 @@
+package training.basic.dataprovider;
+
+public class SearchTestData {
+    private String searchText;
+
+    public SearchTestData(String searchText){
+        this.searchText = searchText;
+    }
+
+    public String getSearchText(){
+        return searchText;
+    }
+}
+
+
