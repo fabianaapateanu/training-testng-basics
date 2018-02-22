b2bdd07886b2fce28795cb0b78157bddc2ac6d9b
cpitaru <cosmin.pitaru@endava.com>
2018-02-22 14:19:58 +0200

    First Homework

---
+++ training-testng-basics/src/main/java/training/basic/dataprovider/Search.java
@@ -0,0 +1,14 @@
+package training.basic.dataprovider;
+
+public class Search {
+    private String searchText;
+
+    public Search(String searchText) {
+        this.searchText = searchText;
+    }
+
+    public String getSearchText() {
+        return searchText;
+    }
+
+}
